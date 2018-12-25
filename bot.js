5afe45f6a711c5b195de892949c1f93c3b884d4c
metodayn <45229634+metodayn@users.noreply.github.com>
2018-12-25 23:08:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,11 +54,11 @@ client2.on('message', message => {
 
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === '!555') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -68,11 +68,11 @@ if (message.content === '!spam') {
 });
 
 client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
+if (message.content === '!555') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER XQ2 IS MY BROTHER **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
